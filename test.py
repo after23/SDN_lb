2c75bb341c8e088e61b137b105444cdd11b051c5
Afif <afiftrifarhan@gmail.com>
2021-04-28 18:47:35 +0700

    asdf

--- test.py
+++ test.py
@@ -107,26 +107,26 @@ class ShareIt(app_manager.RyuApp):
 		
 			#fetch all details of the event
 		msg = ev.msg
-		   	datapath = msg.datapath
-	   		ofproto = datapath.ofproto
-	   		parser = datapath.ofproto_parser
-	   		in_port = msg.match['in_port']
+		datapath = msg.datapath
+	   	ofproto = datapath.ofproto
+	   	parser = datapath.ofproto_parser
+	   	in_port = msg.match['in_port']
 		dpid = datapath.id
 
-	   		pkt = packet.Packet(msg.data)
-	   		eth = pkt.get_protocols(ethernet.ethernet)[0]
+	   	pkt = packet.Packet(msg.data)
+	   	eth = pkt.get_protocols(ethernet.ethernet)[0]
 
 
-			dst = eth.dst
-	   		src = eth.src
+		dst = eth.dst
+	   	src = eth.src
 
-			dpid = datapath.id
-			self.mac_to_port.setdefault(dpid, {})
+		dpid = datapath.id
+		self.mac_to_port.setdefault(dpid, {})
 
-			self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)		
+		self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)		
 
 		# learn a mac address to avoid FLOOD next time.
-				self.mac_to_port[dpid][src] = in_port	   		
+		self.mac_to_port[dpid][src] = in_port	   		
 		self.logger.info("Ether Type: %s", eth.ethertype)
 		if eth.ethertype == ether_types.ETH_TYPE_LLDP:
 			# ignore lldp packet
@@ -149,41 +149,41 @@ class ShareIt(app_manager.RyuApp):
 				return
 			else:
 				dst = eth.dst
-							src = eth.src
+				src = eth.src
 
-							dpid = datapath.id
-							self.mac_to_port.setdefault(dpid, {})
+				dpid = datapath.id
+				self.mac_to_port.setdefault(dpid, {})
 
-							self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+				self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
-							# learn a mac address to avoid FLOOD next time.
-							self.mac_to_port[dpid][src] = in_port
+				# learn a mac address to avoid FLOOD next time.
+				self.mac_to_port[dpid][src] = in_port
 
-							if dst in self.mac_to_port[dpid]:
-									out_port = self.mac_to_port[dpid][dst]
-							else:
-									out_port = ofproto.OFPP_FLOOD
+				if dst in self.mac_to_port[dpid]:
+					out_port = self.mac_to_port[dpid][dst]
+				else:
+					out_port = ofproto.OFPP_FLOOD
 
-					   		actions = [parser.OFPActionOutput(out_port)]
+				actions = [parser.OFPActionOutput(out_port)]
 
-							# install a flow to avoid packet_in next time
-							if out_port != ofproto.OFPP_FLOOD:
-									match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-									# verify if we have a valid buffer_id, if yes avoid to send both
-									# flow_mod & packet_out
-									if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-											self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-											return
-									else:
-											self.add_flow(datapath, 1, match, actions)
-							data = None
-							if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-									data = msg.data
+				# install a flow to avoid packet_in next time
+				if out_port != ofproto.OFPP_FLOOD:
+					match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+					# verify if we have a valid buffer_id, if yes avoid to send both
+					# flow_mod & packet_out
+					if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+						self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+						return
+					else:
+						self.add_flow(datapath, 1, match, actions)
+					data = None
+					if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+						data = msg.data
 
-							out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-								  	in_port=in_port, actions=actions, data=data)
-							datapath.send_msg(out)
-					   		return
+				out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+					in_port=in_port, actions=actions, data=data)
+					datapath.send_msg(out)
+					return
 	
 		
 		try:
