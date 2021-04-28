2b404ead276520661ee7215bb06efa6db03ea6b2
Afif <afiftrifarhan@gmail.com>
2021-04-28 18:53:38 +0700

    asdf

--- test.py
+++ test.py
@@ -188,44 +188,42 @@ class ShareIt(app_manager.RyuApp):
 		
 		try:
 			if pkt.get_protocols(icmp.icmp)[0]:
-		
 			#if ip_head.proto == inet.IPPROTO_ICMP:
 				dst = eth.dst
-					src = eth.src
+				src = eth.src
 
-					dpid = datapath.id
-					self.mac_to_port.setdefault(dpid, {})
+				dpid = datapath.id
+				self.mac_to_port.setdefault(dpid, {})
 
-					self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+				self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
-					# learn a mac address to avoid FLOOD next time.
-					self.mac_to_port[dpid][src] = in_port
+				# learn a mac address to avoid FLOOD next time.
+				self.mac_to_port[dpid][src] = in_port
 
-					if dst in self.mac_to_port[dpid]:
-						out_port = self.mac_to_port[dpid][dst]
-					else:
-							out_port = ofproto.OFPP_FLOOD
+				if dst in self.mac_to_port[dpid]:
+					out_port = self.mac_to_port[dpid][dst]
+				else:
+					out_port = ofproto.OFPP_FLOOD
 	
-					actions = [parser.OFPActionOutput(out_port)]
+				actions = [parser.OFPActionOutput(out_port)]
 
-					# install a flow to avoid packet_in next time
-					if out_port != ofproto.OFPP_FLOOD:
-							match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-							# verify if we have a valid buffer_id, if yes avoid to send both
-							# flow_mod & packet_out
-							if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-								self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-								return
-							else:
-								self.add_flow(datapath, 1, match, actions)
+				# install a flow to avoid packet_in next time
+				if out_port != ofproto.OFPP_FLOOD:
+					match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+					# verify if we have a valid buffer_id, if yes avoid to send both
+					# flow_mod & packet_out
+					if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+						self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+						return
+					else:
+						self.add_flow(datapath, 1, match, actions)
 					data = None
 					if msg.buffer_id == ofproto.OFP_NO_BUFFER:
 							data = msg.data
 
-					out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-								  in_port=in_port, actions=actions, data=data)
+					out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=data)
 					datapath.send_msg(out)
-				return
+			return
 
 		except:
 			pass
