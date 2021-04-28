9a82e55d71a05fad9330f3ec83c6bc456b850a0a
Afif <afiftrifarhan@gmail.com>
2021-04-28 18:51:27 +0700

    asdf

--- test.py
+++ test.py
@@ -180,9 +180,9 @@ class ShareIt(app_manager.RyuApp):
 					if msg.buffer_id == ofproto.OFP_NO_BUFFER:
 						data = msg.data
 
-				    out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-				    in_port=in_port, actions=actions, data=data)
-				    datapath.send_msg(out)
+					out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+					in_port=in_port, actions=actions, data=data)
+					datapath.send_msg(out)
 					return
 	
 		
