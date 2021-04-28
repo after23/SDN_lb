32875b9be9d6374da6003a0d7afb5bd4e121599a
Afif <afiftrifarhan@gmail.com>
2021-04-28 13:23:30 +0700

    test

---
+++ Final/lb.py
@@ -0,0 +1,173 @@
+# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+
+
+class SimpleSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    virtual_ip = "10.0.0.10"
+
+    ser1_mac = "00:00:00:11:11:11"
+    ser1_ip = "10.0.0.11"
+    ser2_mac = "00:00:00:22:22:22"
+    ser2_ip = "10.0.0.22"
+    ser3_mac = "00:00:00:33:33:33"
+    ser3_ip = "10.0.0.33"
+    next_ser = ""
+    cur_ser = ""
+
+    ser_ip_to_port = {ser1_ip: 1, ser2_ip: 2, ser3_ip:3}
+    ip_to_mac = {"10.0.0.1": "00:00:00:00:01",
+                 "10.0.0.2": "00:00:00:00:02",
+                 "10.0.0.3": "00:00:00:00:03",
+                 "10.0.0.4": "00:00:00:00:04"
+                }
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+        self.next_ser = self.ser1_ip
+        self.cur_ser = self.ser1_ip
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install table-miss flow entry
+        #
+        # We specify NO BUFFER to max_len of the output action due to
+        # OVS bug. At this moment, if we specify a lesser number, e.g.,
+        # 128, OVS will send Packet-In with invalid buffer_id and
+        # truncated packet data. In that case, we cannot output packets
+        # correctly.  The bug has been fixed in OVS v2.1.0.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow_miss(datapath, 0, match, actions)
+
+    def add_flow_miss(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    def add_flow_lb(self, datapath, packet, ofp_parser, ofp, in_port):
+        srcIp = packet.get_protocol(arp.arp).src_ip
+
+        # Don't push forwarding rules if an ARP request is received from a server.
+        if srcIp == self.ser1_ip or srcIp == self.ser2_ip, or srcIp == self.ser3_ip:
+            return
+
+        # Generate flow from host to server.
+        match = ofp_parser.OFPMatch(in_port=in_port,
+                                    ipv4_dst=self.virtual_ip,
+                                    eth_type=0x0800)
+        actions = [ofp_parser.OFPActionSetField(ipv4_dst=self.cur_ser),
+                   ofp_parser.OFPActionOutput(self.ip_to_port[self.cur_ser])]
+        inst = [ofp_parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions)]
+        
+        mod = ofp_parser.OFPFlowMod(
+            datapath=datapath,
+            priority=0,
+            buffer_id=ofp.OFP_NO_BUFFER,
+            match=match,
+            instructions=inst)
+
+        datapath.send_msg(mod)
+
+        # Generate reverse flow from server to host.
+        match = ofp_parser.OFPMatch(in_port=self.ser_ip_to_port[self.cur_ser],
+                                    ipv4_src=self.cur_ser,
+                                    ipv4_dst=srcIp,
+                                    eth_type=0x0800)
+        actions = [ofp_parser.OFPActionSetField(ipv4_src=self.virtual_ip),
+                   ofp_parser.OFPActionOutput(in_port)]
+        inst = [ofp_parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions)]
+
+        mod = ofp_parser.OFPFlowMod(
+            datapath=datapath,
+            priority=0,
+            buffer_id=ofp.OFP_NO_BUFFER,
+            match=match,
+            instructions=inst)
+
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)
+
+        if eth.ethertype == ether_types.ETH_TYPE_ARP:
+            self.add_flow_lb(datapath, pkt, parser, in_port)
+            self.arp_response(datapath, pkt, eth, parser, ofproto, in_port)
+            self.cur_ser = self.next_ser
+            return
+        else:
+            return
+
+        def arp_response(self, datapath, packet, eth, parser, ofproto, in_port):
+            arp_packet = packet.get_protocol(arp.arp)
+            dstIp = arp_packet.src_ip
+            srcIp = arp_packet.dst_ip
+            dstMac = eth.src
+
+            if dstIp != self.ser1_ip and dstIp != self.ser2.ip and dstIp != self.ser3_ip:
+                if self.next_ser == self.ser1_ip:
+                    srcMac = self.ser1_mac
+                    self.next_ser = self.ser2_ip
+                elif self.next_ser == self.ser2_ip:
+                    srcMac = self.ser2_mac
+                else:
+                    srcMac = self.ser3_mac
+            else:
+                srcMac = self.ip_to_mac[srcIp]
+
+            e = ethernet.ethernet(dstMac, srcMac, ether_types.ETH_TYPE_ARP)
+            a = arp.arp(1, 0x0800, 6, 4, 2, srcMac, srcIp, dstMac, dstIp)
+            p = packet()
+            p.add_protocol(e)
+            p.add_protocol(a)
+            p.serialize()
+
+            actions= [ofp_parser.OFPActionOutput(ofp.OFPP_IN_PORT)]
+            out = ofp_parser.OFPPacketOut(datapath=datapath, buffer_id = OFP_NO_BUFFER, in_port=in_port, actions = actions, data=p.data)
+            datapath.send_msg(out)
--- lb_topo.py
+++ lb_topo.py
@@ -32,32 +32,35 @@ from mininet.node import OVSSwitch, Controller, RemoteController
 from time import sleep
 
 
-class SingleSwitchTopo(Topo):
+class toplogi(Topo):
     "Single switch connected to n hosts."
     def build(self):
         s1 = self.addSwitch('s1', protocols='OpenFlow13')
-        s2 = self.addSwitch('s2', protocols='OpenFlow13')
-        s3 = self.addSwitch('s3', protocols='OpenFlow13')
-        s4 = self.addSwitch('s4', protocols='OpenFlow13')
-        s5 = self.addSwitch('s5', protocols='OpenFlow13')
 
-        h1 = self.addHost('h1', mac="00:00:00:00:00:01", ip="192.168.1.1/24")
-        h2 = self.addHost('h2', mac="00:00:00:00:00:02", ip="192.168.1.2/24")
+
+        h1 = self.addHost('h1', mac="00:00:00:11:11:11", ip="10.0.0.11/24")
+        h2 = self.addHost('h2', mac="00:00:00:22:22:22", ip="10.0.0.22/24")
+        h3 = self.addHost('h3', mac="00:00:00:33:33:33", ip="10.0.0.33/24")
+        h4 = self.addHost('h4', mac="00:00:00:00:00:01", ip="10.0.0.1/24")
+        h5 = self.addHost('h5', mac="00:00:00:00:00:02", ip="10.0.0.2/24")
+        h6 = self.addHost('h6', mac="00:00:00:00:00:03", ip="10.0.0.3/24")
+        h7 = self.addHost('h7', mac="00:00:00:00:00:04", ip="10.0.0.4/24")
+
         
-        self.addLink(s1,s2,1,1)
-        self.addLink(s1,s3,2,1) 
-        self.addLink(s1,h1,3,1)
+        self.addLink(s1,h1,1,1)
+        self.addLink(s1,h2,2,1) 
+        self.addLink(s1,h3,3,1)
 
 
-        self.addLink(s3,s5,2,2)
-        self.addLink(s4,s2,1,2)
-        self.addLink(s4,s5,2,1)
-    
-        self.addLink(s5,h2,3,1)
+        self.addLink(s1,h4,4,1)
+        self.addLink(s1,h5,5,1)
+        self.addLink(s1,h6,6,1)
+        self.addLink(s1,h7,7,1)
+        
 
 if __name__ == '__main__':
     setLogLevel('info')
-    topo = SingleSwitchTopo()
+    topo = topologi()
     c1 = RemoteController('c1', ip='127.0.0.1')
     net = Mininet(topo=topo, controller=c1)
     net.start()
