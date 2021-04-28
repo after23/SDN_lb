226c51538e0740f43fb8089da32a217275a294a8
Afif <afiftrifarhan@gmail.com>
2021-04-28 19:20:40 +0700

    asdf

--- test.py
+++ test.py
@@ -217,19 +217,15 @@ class ShareIt(app_manager.RyuApp):
 						return
 					else:
 						self.add_flow(datapath, 1, match, actions)
-					data = None
-					if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-							data = msg.data
-
-					out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=data)
-					datapath.send_msg(out)
-			return
+				data = None
+				if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+				data = msg.data
+				out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=data)
+				datapath.send_msg(out)
+				return
 
 		except:
 			pass
-		
-
-
 		ip_head = pkt.get_protocols(ipv4.ipv4)[0]
 		tcp_head = pkt.get_protocols(tcp.tcp)[0]
 
